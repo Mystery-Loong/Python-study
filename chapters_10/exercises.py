c1a1231c14532b6bbff135122203aeade275426a
Mystery Loong <15277112806@139.com>
2025-07-24 17:57:56 +0800

    wsl update data 7.24-2

--- chapters_10/exercises.py
+++ chapters_10/exercises.py
@@ -1,10 +1,24 @@
 # 10.1
+# from pathlib import Path
+
+# path = Path('learning_python.txt')
+# contents = path.read_text()
+
+# print(contents)
+
+# lines = contents.splitlines()
+# for line in lines:
+#     print(line)
+
+# 10.2
 from pathlib import Path
 
 path = Path('learning_python.txt')
 contents = path.read_text()
+contents = contents.replace('Python','R')
 
 print(contents)
 
-lines = contents.splitlines().lstrip()
+lines = contents.splitlines()
 for line in lines:
+    print(line)
