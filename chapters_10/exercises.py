6cfd37d38f88cfe584b6c2b16ce06add6d0a7bab
Mystery Loong <15277112806@139.com>
2025-07-25 16:10:08 +0800

    wsl update data 7.25

--- chapters_10/exercises.py
+++ chapters_10/exercises.py
@@ -11,14 +11,37 @@
 #     print(line)
 
 # 10.2
+# from pathlib import Path
+
+# path = Path('learning_python.txt')
+# contents = path.read_text()
+# contents = contents.replace('Python','R')
+
+# print(contents)
+
+# lines = contents.splitlines()
+# for line in lines:
+#     print(line)
+
+# 10.4
+# from pathlib import Path
+
+# path = Path('guest.txt')
+# contents = input("Please input you name :")
+
+# path.write_text(contents)
+
+# 10.5
 from pathlib import Path
 
-path = Path('learning_python.txt')
-contents = path.read_text()
-contents = contents.replace('Python','R')
-
+path = Path('guest_book.txt')
+contents = "name:\n"
+while True:
+    print("Enter 'q' at any time to quit.")
+    content = input("Please input you name:")
+    if content == 'q':
+        break
+    else:
+        contents += f"\t{content}\n"
+path.write_text(contents)
 print(contents)
-
-lines = contents.splitlines()
-for line in lines:
-    print(line)
