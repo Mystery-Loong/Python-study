d47f23b99538c268647ee0229aa37ea24bfd2ca2
Mystery Loong <15277112806@139.com>
2025-07-25 23:21:30 +0800

    wsl updata data 7.25-2

--- chapters_10/word_count.py
+++ chapters_10/word_count.py
@@ -4,12 +4,15 @@ def count_words(path):
     try:
         contents = path.read_text(encoding='utf-8')
     except FileNotFoundError:
-        print(f"Sorry,the file {path} does not exist.")
+        # print(f"Sorry,the file {path} does not exist.")
+        pass
     else:
         # 计算文件包含多少个单词
         words = contents.split()
         num_words = len(words)
         print(f"The file {path} has about {num_words} words.")
 
-    path = Path('alice.txt')
+filenames = ['alice.txt','siddhartha.txt','Moby_Dick.txt']
+for filename in filenames:
+    path = Path(filename)
     count_words(path)
