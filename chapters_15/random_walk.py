60584fb7591f00916de14eaea4bea3d16c9137f4
Mystery-Loong <15277112806@139.com>
2025-08-09 19:20:48 +0800

    ubuntu update data 8.9

--- chapters_15/random_walk.py
+++ chapters_15/random_walk.py
@@ -16,14 +16,8 @@ class RandomWalk:
         # 不断游走，直到列表达到指定的长度
         while len(self.x_values) < self.num_points:
 
-            # 决定前进的方向以及沿这个方向前进的距离
-            x_direction = choice([1,-1])
-            x_distance = choice([0,1,2,3,4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1,-1])
-            y_distance = choice([0,1,2,3,4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # 拒绝原地踏步
             if x_step == 0 and y_step ==0:
@@ -35,3 +29,12 @@ class RandomWalk:
 
             self.x_values.append(x)
             self.y_values.append(y)
+
+    def get_step(self):
+        """确定方向和距离，并返回"""
+
+        # 决定前进的方向以及沿这个方向前进的距离
+        direction = choice([1,-1])
+        distance = choice([0,1,2,3,4,5,6,7,8])
+        step = direction * distance
+        return step
